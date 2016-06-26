83a45183397d0c77e8e4bd030376264c84e9bac4
natakin2016 <natakin2016@gmail.com>
2016-06-26 16:54:22 -0600

    Add files via upload

--- slap-game/code.js
+++ slap-game/code.js
@@ -1,22 +1,30 @@
-var health = 100;
-var y=['','oof!!','ouch!!','gahh!!','not pleasent!!','really!?!','thats going to hurt tomorrow...','was that necessary!!?'];
+var damageValue= 0;
+var coins = 0;
+var monsterHealth=100
+
 
 function damage(x){
-  health=health-x;
-  var n=Math.floor((Math.random() * 7) + 1  );
-  if(health>0){
-    document.getElementById("health").innerHTML= health;
-    document.getElementById("sound").innerHTML= y[n].toUpperCase();
+  totalDamage=x+damageValue;
+  monsterHealth=monsterHealth-totalDamage ;
+  if(monsterHealth>0){
+    document.getElementById("damagePoints").innerHTML= "Damage: "+ (totalDamage);
+    document.getElementById("monsterHealth").innerHTML="Health: "+(monsterHealth)
   }
   else{
-    alert("Awww Bummer You Died".toUpperCase());
-    document.getElementById("health").innerHTML="0";
-    document.getElementById("dummy").className="dummyhalf"
+    coins=coins+5
+    //  alert("In a stunning turn of events you managed to kill the Monster!")
+     document.getElementById("img").innerHTML= '<img height="200px" src="death.png">'
+     document.getElementById("monsterHealth").innerHTML="Health: 0"
+     document.getElementById("coins").innerHTML="Coins: "+ coins
+     alert("In a stunning turn of events you managed to kill the Monster!")
   }
 }
 
+function reset(){
+  damageValue= 0;
+  coins = 0;
+  monsterHealth = 100;
+  document.getElementById("monsterHealth").innerHTML="Health: "+(monsterHealth)
 
-function restart(){
-  health=100;
-  document.getElementById("health").innerHTML= health;
 }
+reset()
